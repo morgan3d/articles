82ccaaabe38d12dc77b9b51f86e031e520248593
Morgan <morgan3d@gmail.com>
2018-01-13 22:44:35 -0500

    span

--- ce-blog.js
+++ ce-blog.js
@@ -1,7 +1,7 @@
 function addBlogRunner() {
     var header = `
-<a style="text-decoration:none" href="https://casual-effects.com"><div style="margin-top:-20px; width:100%; height: 60px; border-bottom:1px solid; background:url('https://casual-effects.com/casual-effects.png'); background-size: contain; background-repeat: no-repeat; background-position: center; "></div></a>`;
-    var footer = `<div style="border-top: solid 1px; padding-top:10px">
+<span class="md"><a style="text-decoration:none" href="https://casual-effects.com"><div style="margin-top:-20px; width:100%; height: 60px; border-bottom:1px solid; background:url('https://casual-effects.com/casual-effects.png'); background-size: contain; background-repeat: no-repeat; background-position: center; "></div></a></span>`;
+    var footer = `<div style="border-top: solid 1px; padding-top:10px" class="md">
 <table><tr valign=top><td><img src="http://casual-effects.com/morgan/headshots/mcguire-square-small.png" width="85px" style="border:1px solid #888; margin-right:25px"></td><td>
 <i>
 This is a personal blog article by <a href="https://casual-effects.com">Morgan
