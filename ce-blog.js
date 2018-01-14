d04a05ff2226bac9e276ee18bc8eb3511c87b01b
Morgan <morgan3d@gmail.com>
2018-01-13 22:58:17 -0500

    .

--- ce-blog.js
+++ ce-blog.js
@@ -2,8 +2,8 @@ function addBlogRunner() {
     var header = `
 <span class="md"><a style="text-decoration:none" href="https://casual-effects.com"><div style="margin-top:-20px; width:100%; height: 60px; border-bottom:1px solid; background:url('https://casual-effects.com/casual-effects.png'); background-size: contain; background-repeat: no-repeat; background-position: center; "></div></a></span>`;
     var footer = `<div style="border-top: solid 1px; padding-top:10px" class="md">
-<table><tr valign=top><td><img src="http://casual-effects.com/morgan/headshots/mcguire-square-small.png" width="85px" style="border:1px solid #888; margin-right:25px"></td><td>
-<i>
+<table><tr valign=top><td><img src="http://casual-effects.com/morgan/headshots/mcguire-square-small.png" width="200px" style="border:1px solid #888"></td><td width="30px"> </td><td>
+<i style="font-size:95%">
 This is a personal blog article by <a href="https://casual-effects.com">Morgan
 McGuire</a>. Morgan is a scientist at NVIDIA and holds faculty appointments at Williams College
 and the University of Waterloo. 
