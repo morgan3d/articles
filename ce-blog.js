ab6dbbea87a5b0dd62430b6328b3f9114bc1ba24
Morgan <morgan3d@gmail.com>
2019-04-16 10:57:52 -0400

    DDGI introduction

--- ce-blog.js
+++ ce-blog.js
@@ -1,5 +1,5 @@
 function addBlogRunner() {
-    var header = `
+    var header = `<style>em.asterisk { font-style: normal; font-weight: bold; }</style>
 <span class="md"><a style="text-decoration:none" href="https://casual-effects.com"><div style="margin-top:-20px; width:100%; height: 60px; border-bottom:1px solid; background:url('https://casual-effects.com/casual-effects.png'); background-size: contain; background-repeat: no-repeat; background-position: center; "></div></a></span>`;
     var footer = `<div style="border-top: solid 1px; padding-top:10px" class="md">
 <table><tr valign=top><td><img src="http://casual-effects.com/morgan/headshots/mcguire-square-small.png" width="200px" style="border:1px solid #888"></td><td width="30px"> </td><td>
