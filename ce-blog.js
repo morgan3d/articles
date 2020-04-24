7cd690d6f5067c9872adc78e62a5a29519707367
Morgan <morgan3d@gmail.com>
2020-04-24 08:57:35 -0400

    git

--- ce-blog.js
+++ ce-blog.js
@@ -2,11 +2,11 @@ function addBlogRunner() {
     var header = `<style>em.asterisk { font-style: normal; font-weight: bold; }</style>
 <span class="md"><a style="text-decoration:none" href="https://casual-effects.com"><div style="margin-top:-20px; width:100%; height: 60px; border-bottom:1px solid; background:url('https://casual-effects.com/casual-effects.png'); background-size: contain; background-repeat: no-repeat; background-position: center; "></div></a></span>`;
     var footer = `<div style="border-top: solid 1px; padding-top:10px" class="md">
-<table><tr valign=top><td><img src="http://casual-effects.com/morgan/headshots/mcguire-square-small.png" width="200px" style="border:1px solid #888"></td><td width="30px"> </td><td>
+<table><tr valign=top><td><img src="https://casual-effects.com/morgan/headshots/mcguire-square-small.png" width="200px" style="border:1px solid #888"></td><td width="30px"> </td><td>
 <i style="font-size:95%">
 This is a personal blog article by <a href="https://casual-effects.com">Morgan
-McGuire</a>. Morgan is a scientist at NVIDIA and holds faculty appointments at Williams College,
-the University of Waterloo, and McGill University.
+McGuire</a>. Morgan is a director of research at NVIDIA and holds faculty appointments at
+the University of Waterloo and McGill University.
 
 <p>He is the author of the <a
 href="http://graphicscodex.com">Graphics Codex</a>, an essential reference for computer
@@ -16,8 +16,11 @@ graphics now available in iOS and Web Editions.</p></i>
     if (document.getElementsByClassName("title").length) {
         document.body.innerHTML = header + document.body.innerHTML + footer;
     } else {
-        setTimeout(addBlogRunner,100);
+        setTimeout(addBlogRunner, 100);
     }
 }
+
+if(!window.markdeepOptions){window.markdeepOptions={tocStyle:'medium'};}
+document.write('<style>h1 {margin-top:50px; padding-bottom:6px; border-bottom: 1px solid #555}em.asterisk { font-style: normal; font-weight: bold; }</style>');
 
 addBlogRunner();
