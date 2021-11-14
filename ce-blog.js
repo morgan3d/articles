61903135d8cc3ef40a9e45ece2b7424a72dc24cf
Morgan <morgan3d@gmail.com>
2021-11-13 22:45:40 -0500

    Updated image link

--- ce-blog.js
+++ ce-blog.js
@@ -2,7 +2,7 @@ function addBlogRunner() {
     var header = `<style>em.asterisk { font-style: normal; font-weight: bold; }</style>
 <span class="md"><a style="text-decoration:none" href="https://casual-effects.com"><div style="margin-top:-20px; width:100%; height: 60px; border-bottom:1px solid; background:url('https://casual-effects.com/casual-effects.png'); background-size: contain; background-repeat: no-repeat; background-position: center; "></div></a></span>`;
     var footer = `<div style="border-top: solid 1px; padding-top:10px" class="md">
-<table><tr valign=top><td><img src="https://casual-effects.com/morgan/headshots/mcguire-square-small.png" width="200px" style="border:1px solid #888"></td><td width="30px"> </td><td>
+<table><tr valign=top><td><img src="https://casual-effects.com/morgan/headshots/mcguire-tiny.jpg" width="200px" style="border:1px solid #888"></td><td width="30px"> </td><td>
 <i style="font-size:95%">
 This is a personal blog article by <a href="https://casual-effects.com">Morgan
 McGuire</a> [<a href="https://twitter.com/CasualEffects">@CasualEffects</a>]. Morgan is the Chief Scientist at Roblox and holds faculty appointments at
