d0caac1d016aa339f8924baa376d414078f97e08
Morgan <3970018+morgan3d@users.noreply.github.com>
2023-01-01 08:00:55 -0500

    Favicon

--- sailing/sailing.js
+++ sailing/sailing.js
@@ -18,6 +18,7 @@ excepting portions credited to other sources. <a rel="license" href="http://crea
 </td></tr></table>
 </div>
 
+<link rel="icon" sizes="32x32" type="image/x-icon" href="${baseURL}/compass-rose-32x32.ico">
 <link rel="shortcut icon" type="image/png" href="${baseURL}/compass-rose.png">
 <link rel="icon" sizes="32x32" type="image/png" href="${baseURL}/compass-rose-32x32.png">
 <style>
