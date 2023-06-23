5c2fdcfc496495b6c0b5a6d6a70125523de2e299
Morgan <3970018+morgan3d@users.noreply.github.com>
2023-06-23 00:01:47 -0400

    FCB1010

--- ce-blog.js
+++ ce-blog.js
@@ -2,15 +2,12 @@ function addBlogRunner() {
     var header = `<style>em.asterisk { font-style: normal; font-weight: bold; }</style>
 <span class="md"><a style="text-decoration:none" href="https://casual-effects.com"><div style="margin-top:-20px; width:100%; height: 60px; border-bottom:1px solid; background:url('https://casual-effects.com/casual-effects.png'); background-size: contain; background-repeat: no-repeat; background-position: center; "></div></a></span>`;
     var footer = `<div style="border-top: solid 1px; padding-top:10px" class="md">
-<table><tr valign=top><td><img src="https://casual-effects.com/morgan/headshots/mcguire-tiny.jpg" style="max-width: none; width: 200px; height: 200px; border:1px solid #888"></td><td width="30px"> </td><td>
+<table><tr valign=top><td><img src="https://casual-effects.com/morgan/headshots/mcguire-tiny.jpg" style="max-width: none; width: 128px; height: 128px; border:1px solid #888"></td><td width="30px"> </td><td>
 <i style="font-size:95%">
 This is a personal blog article by <a href="https://casual-effects.com">Morgan
 McGuire</a> [<a href="https://twitter.com/CasualEffects">@CasualEffects</a>]. Morgan is the Chief Scientist at Roblox and holds faculty appointments at
 the University of Waterloo and McGill University.
-
-<p>He is the author of the <a
-href="https://graphicscodex.com">Graphics Codex</a>, an essential reference for computer
-graphics now available in iOS and Web Editions.</p></i>
+</i>
 </td></tr></table>
 </div>`;
     if (document.getElementsByClassName("title").length) {
